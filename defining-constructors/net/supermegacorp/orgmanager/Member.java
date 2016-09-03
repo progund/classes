2f0256a0e51f77f2a77682c7b0ce798c36550a14
Henrik Sandklef <hesa@sandklef.com>
2016-09-03 13:38:19 +0200

    first rev

---
+++ defining-constructors/net/supermegacorp/orgmanager/Member.java
@@ -0,0 +1,12 @@
+package net.supermegacorp.orgmanager;
+
+public class Member {
+
+    private String name;
+    private String email;
+    
+    public String toString() {
+        return name + " " + email;
+    }
+    
+}
---
+++ defining-constructors/net/supermegacorp/orgmanager/test/MemberTest.java
@@ -0,0 +1,17 @@
+package net.supermegacorp.orgmanager.test;
+
+import net.supermegacorp.orgmanager.Member;
+
+public class MemberTest {
+
+    public static void main(String[] args) {
+
+        Member m = new Member();
+        m.name = "Ada";
+        //        m.email = "ada@lovelace.net";
+        System.out.println(m);
+        //System.out.println(m.name);
+
+    }
+    
+}
