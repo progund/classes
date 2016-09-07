c58b6783eed9b38c4851e41c42ed068bc8339504
Henrik Sandklef <hesa@sandklef.com>
2016-09-07 14:08:03 +0200

    first rev

---
+++ comments/org/commenteers/Example.java
@@ -0,0 +1,17 @@
+package org.commenteers;
+
+/*
+  This class is just a simple example
+  on how to comment code in Java
+  
+ */
+
+public class Example{ // Compier ignores this
+    
+    // main method, starting point
+    public static void main(String[] args) {
+        // Print a welcome message
+        System.out.println("Welcome to the example class ... ");
+    }     
+  
+}
