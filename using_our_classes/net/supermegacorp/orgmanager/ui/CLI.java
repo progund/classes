ff9d1b3311d87093fcf7c0929f67eef17335d4ab
Henrik Sandklef <hesa@sandklef.com>
2017-10-01 21:08:19 +0200

    Format with java-format.sh

--- using_our_classes/net/supermegacorp/orgmanager/ui/CLI.java
+++ using_our_classes/net/supermegacorp/orgmanager/ui/CLI.java
@@ -1,83 +1,77 @@
 package net.supermegacorp.orgmanager.ui;
 
-import net.supermegacorp.orgmanager.domain.Member;
-import java.io.PrintStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
+import net.supermegacorp.orgmanager.domain.Member;
 
-public class CLI{
+public class CLI {
 
-    /*
-     * Private variables used when reading/writing from/to user
-     *
-     * We're using *Stream to make it easier to replace stdout/stdin
-     * with other streams in the future
-     *
-     */
-    private PrintStream out      = System.out;
-    private InputStream in       = System.in;
-    private Scanner     scanner  = new Scanner(in);
+  /*
+   * Private variables used when reading/writing from/to user
+   *
+   * We're using *Stream to make it easier to replace stdout/stdin
+   * with other streams in the future
+   *
+   */
+  private PrintStream out = System.out;
+  private InputStream in = System.in;
+  private Scanner scanner = new Scanner(in);
 
-    /*
-     * 
-     * Simply outputs the String phrase - without a trailing newline
-     *
-     */
-    public void msg(String phrase){
-        out.print(phrase);
-    }
-    
+  /*
+   *
+   * Simply outputs the String phrase - without a trailing newline
+   *
+   */
+  public void msg(String phrase) {
+    out.print(phrase);
+  }
 
-    /*
-     * 
-     * Simply outputs the String phrase - with a trailing newline
-     *
-     */
-    public void msgln(String phrase){
-        out.println(phrase);
-    }
-    
+  /*
+   *
+   * Simply outputs the String phrase - with a trailing newline
+   *
+   */
+  public void msgln(String phrase) {
+    out.println(phrase);
+  }
 
-    /*
-     * Outputs the String phrase (without a trailing newline)
-     * and reads a line from user. The line is returned.
-     *
-     */
-    public String askUser(String phrase){
-        msg(phrase);
-        return scanner.nextLine();
-    }
-    
-    /*
-     * Reads a line from user. The line is returned.
-     *
-     */
-    public String askUser() {
-        return scanner.nextLine();
-    }
+  /*
+   * Outputs the String phrase (without a trailing newline)
+   * and reads a line from user. The line is returned.
+   *
+   */
+  public String askUser(String phrase) {
+    msg(phrase);
+    return scanner.nextLine();
+  }
 
-    /*
-     * Asks the user to enter the required fields, creates a new
-     * Member instance and returns that member instance.
-     *
-     */
-    public Member readNewMember() {
-        msgln("Enter values for new Member");
-        String name  = askUser("* name: "); 
-        String email = askUser("* email: "); 
-        Member m     = new Member(name, email);
-        return m;
-    }
+  /*
+   * Reads a line from user. The line is returned.
+   *
+   */
+  public String askUser() {
+    return scanner.nextLine();
+  }
 
-    /*
-     * Outputs a Member instance 
-     *
-     */
-    public void outputMember(Member m) {
-        msgln("Member: " + m);
-    }
-    
+  /*
+   * Asks the user to enter the required fields, creates a new
+   * Member instance and returns that member instance.
+   *
+   */
+  public Member readNewMember() {
+    msgln("Enter values for new Member");
+    String name = askUser("* name: ");
+    String email = askUser("* email: ");
+    Member m = new Member(name, email);
+    return m;
+  }
 
-    
-    
+  /*
+   * Outputs a Member instance
+   *
+   */
+  public void outputMember(Member m) {
+    msgln("Member: " + m);
+  }
 }
--- using_our_classes/net/supermegacorp/orgmanager/ui/MemberManager.java
+++ using_our_classes/net/supermegacorp/orgmanager/ui/MemberManager.java
@@ -1,24 +1,20 @@
 package net.supermegacorp.orgmanager.ui;
 
-import net.supermegacorp.orgmanager.domain.Member;
-import net.supermegacorp.orgmanager.ui.CLI;
-import java.util.List;
-import java.util.ArrayList;
-
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.IOException;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import net.supermegacorp.orgmanager.domain.Member;
 
-
-public class MemberManager{
+public class MemberManager {
 
   private CLI cli;
   private List<Member> members;
-  private String membersFile="members.data";
+  private String membersFile = "members.data";
 
-  
   public MemberManager() {
     cli = new CLI();
     members = new ArrayList<Member>();
@@ -28,14 +24,14 @@ public class MemberManager{
   public void welcome() {
     cli.msgln("Starting Member manager");
   }
-  
+
   public void goodbye() {
     cli.msgln("Good bye");
   }
-  
+
   public void menuLoop() {
-    /* 
-     * Add some kind of loop here - hint, use while 
+    /*
+     * Add some kind of loop here - hint, use while
      */
 
     /*
@@ -45,17 +41,17 @@ public class MemberManager{
     cli.msgln(" 1 - to add member");
     cli.msgln(" 2 - to list members");
     cli.msgln(" 3 - to quit");
-    
+
     /*
      * Read command from user
      */
     String command = cli.askUser("Enter your choice");
-    
+
     /*
      * Based on command, take different actions
      */
     if (command.equals("1")) {
-      Member m   = cli.readNewMember();
+      Member m = cli.readNewMember();
       cli.msgln("Created member: " + m);
       /*
        * add the member to the ArrayList
@@ -63,13 +59,13 @@ public class MemberManager{
     } else if (command.equals("2")) {
       cli.msgln("No members - you need to add code to add member");
       cli.msgln("and then code to print 'em out");
-      /* loop through the ArrayList and print the members, 
+      /* loop through the ArrayList and print the members,
          hint: the CLI class has an (instance) method called outputMember
       */
-      
+
       for (Member m : members) {
         cli.outputMember(m);
-      }        
+      }
     } else if (command.equals("3")) {
       cli.msgln("Good bye...");
       saveToFile();
@@ -81,30 +77,29 @@ public class MemberManager{
     } else {
       cli.msgln("Invalid choice?");
     }
-    
   }
-  
-  // 
+
+  //
   // Below you'll find methods dealing with storing and reading back
   // users from a file (members.data)
   //
-  
-  private void saveToFile(){
-    try{
-      FileOutputStream fos= new FileOutputStream(membersFile);
-      ObjectOutputStream oos= new ObjectOutputStream(fos);
+
+  private void saveToFile() {
+    try {
+      FileOutputStream fos = new FileOutputStream(membersFile);
+      ObjectOutputStream oos = new ObjectOutputStream(fos);
       oos.writeObject(members);
       oos.close();
       oos.flush();
       fos.flush();
       fos.close();
-    }catch(IOException e){
+    } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
   @SuppressWarnings({"unchecked"})
-  private void readFromFile(){
+  private void readFromFile() {
     try {
       FileInputStream fis = new FileInputStream(membersFile);
       ObjectInputStream ois = new ObjectInputStream(fis);
@@ -112,18 +107,16 @@ public class MemberManager{
       members = (ArrayList) readObject;
       ois.close();
       fis.close();
-    } catch(java.io.FileNotFoundException c){
+    } catch (java.io.FileNotFoundException c) {
       cli.msgln("File not found");
       return;
-    }catch(IOException ioe){
+    } catch (IOException ioe) {
       ioe.printStackTrace();
       return;
-    }catch(ClassNotFoundException c){
+    } catch (ClassNotFoundException c) {
       cli.msgln("Class not found");
       c.printStackTrace();
       return;
     }
-    
   }
-  
 }
