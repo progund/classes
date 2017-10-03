208d0eba7226c1a91c3f716f930f2960acc8dd37
Rikard Fröberg <rikard@morus.se>
2017-10-03 21:26:19 +0200

    Switched to Javadoc style comments

--- using_our_classes/net/supermegacorp/orgmanager/ui/CLI.java
+++ using_our_classes/net/supermegacorp/orgmanager/ui/CLI.java
@@ -5,6 +5,15 @@ import java.io.PrintStream;
 import java.util.Scanner;
 import net.supermegacorp.orgmanager.domain.Member;
 
+/**
+ * Represents an object for a command line interface with the user.
+ *
+ * This class can be used to create an object which lets you interface
+ * with the user via a text-based interface, using Stdin and Stdout.
+ *
+ * The object has methods for promting the user with a question and
+ * returning the user's input via the command line interactively.
+ */
 public class CLI {
 
   /*
@@ -18,46 +27,53 @@ public class CLI {
   private InputStream in = System.in;
   private Scanner scanner = new Scanner(in);
 
-  /*
+  /**
+   * Simply outputs a String without a trailing newline, effectively
+   * wrapping System.out.print()
    *
-   * Simply outputs the String phrase - without a trailing newline
-   *
+   * @param phrase The String to output
    */
   public void msg(String phrase) {
     out.print(phrase);
   }
 
-  /*
+  /**
+   * Simply outputs a String phrase with a trailing newline,
+   * effectively wrapping System.out
    *
-   * Simply outputs the String phrase - with a trailing newline
-   *
+   * @param phrase The String to output with a trailing newline
    */
   public void msgln(String phrase) {
     out.println(phrase);
   }
 
-  /*
-   * Outputs the String phrase (without a trailing newline)
-   * and reads a line from user. The line is returned.
+  /**
+   * Prompts the user with a String (without a trailing newline)
+   * and reads a line from user.
    *
+   * @param phrase The String to prompt the user with
+   * @return The String read from the user (the user's reply)
    */
   public String askUser(String phrase) {
     msg(phrase);
     return scanner.nextLine();
   }
 
-  /*
+  /**
    * Reads a line from user. The line is returned.
    *
+   * @return The String the user enters interactively
    */
   public String askUser() {
     return scanner.nextLine();
   }
 
-  /*
+  /**
    * Asks the user to enter the required fields, creates a new
    * Member instance and returns that member instance.
    *
+   * @return A reference to a new Member, created with the user's input
+   * as arguments to the Member constructor
    */
   public Member readNewMember() {
     msgln("Enter values for new Member");
@@ -67,9 +83,11 @@ public class CLI {
     return m;
   }
 
-  /*
-   * Outputs a Member instance
+  /**
+   * Outputs a Member instance. This is equivalent to calling
+   * System.out.println(m) where m is a reference to a Member instance.
    *
+   * @param m The member to output (with a trailing newline).
    */
   public void outputMember(Member m) {
     msgln("Member: " + m);
