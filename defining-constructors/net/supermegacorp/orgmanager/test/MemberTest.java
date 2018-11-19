0e1645faf0b618cc8a70052406d9867d730dfe72
Rikard Fröberg <rikard@morus.se>
2018-11-19 23:43:13 +0100

    fixes code style

--- defining-constructors/net/supermegacorp/orgmanager/test/MemberTest.java
+++ defining-constructors/net/supermegacorp/orgmanager/test/MemberTest.java
@@ -4,17 +4,14 @@ import net.supermegacorp.orgmanager.Member;
 
 public class MemberTest {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-      Member ada     = new Member("Ada", "ada@lovelace.edu");
-      System.out.println(ada);
+    Member ada = new Member("Ada", "ada@lovelace.edu");
+    System.out.println(ada);
 
-      Member charles = new Member("Charles", "charles __AT__ babbage.net");
-      System.out.println(charles);
-
-
-
+    Member charles = new Member("Charles", "charles __AT__ babbage.net");
+    System.out.println(charles);
       
-    }
+  }
     
 }
